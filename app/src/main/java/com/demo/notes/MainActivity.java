d6ff5f53748e6e958db1970a020a2e68a80e8cb3
Nikita <fill-specialguy@mail.ru>
2020-09-13 21:39:04 +0300

    FirstAndroidApp

---
+++ app/src/main/java/com/demo/notes/MainActivity.java
@@ -0,0 +1,34 @@
+package com.demo.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycleViewNotes);
+        notes.add(new Note("Парикмахер", "Сделать прическу", "Понедельник", 2));
+        notes.add(new Note("Магазин", "Сделать прическу", "Понедельник", 1));
+        notes.add(new Note("Секс-шоп", "Сделать прическу", "Понедельник", 3));
+        notes.add(new Note("Аляска", "Сделать прическу", "Понедельник", 4));
+        notes.add(new Note("Марцепан", "Сделать прическу", "Понедельник", 5));
+        notes.add(new Note("Карандаш", "Сделать прическу", "Понедельник", 6));
+        notes.add(new Note("Диплом", "Сделать прическу", "Понедельник", 7));
+        notes.add(new Note("Мольберт", "Сделать прическу", "Понедельник", 8));
+        notes.add(new Note("Эйнштейн", "Сделать прическу", "Понедельник", 9));
+        notes.add(new Note("Граццио", "Сделать прическу", "Понедельник", 10));
+        NotesAdapter adapter = new NotesAdapter(notes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/demo/notes/NotesAdapter.java
@@ -0,0 +1,56 @@
+package com.demo.notes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
+    private ArrayList<Note> notes;
+
+    public NotesAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        return new NotesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.textViewTitle.setText(note.getTitle());
+        holder.textViewDescription.setText(note.getDescription());
+        holder.textViewViewDayOfWeek.setText(note.getDayOfWeek());
+        holder.textViewTitle.setText(String.format("%s", note.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class NotesViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewViewDayOfWeek;
+        private TextView textViewPriority;
+
+        public NotesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            textViewViewDayOfWeek = itemView.findViewById(R.id.textViewDayOfWeek);
+            textViewPriority = itemView.findViewById(R.id.textViewPriority);
+        }
+    }
+}
